a61f11c676686b0dc1d77bbcc903c68599cf6a49
singseraphim <35641086+singseraphim@users.noreply.github.com>
2021-01-27 12:45:36 -0700

    Streamlined testing

--- main.py
+++ main.py
@@ -8,24 +8,41 @@ import sklearn.ensemble._forest
 import pandas as pd
 import numpy as np
 
+files = {
+    "cnp_10": "cnp_10.txt",
+    "cnp_12": "cnp_12.txt",
+    "cnp_17": "cnp_17.txt",
+    "cnp_21": "cnp_21.txt",
+    "cnp_22": "cnp_22.txt",
+    "cnp_24": "cnp_24.txt",
+    "cnp_27": "cnp_27.txt",
+    "cnp_28": "cnp_28.txt"
+
+}
+
 def main():
-    with open('test_files/json_sessions_104/cnp_24_json_session.txt', 'r') as file:
+
+    # Tests can be run all at once, or one at a time.
+    run_all_tests()
+    run_test("cnp_22")
+
+
+# Runs the given file through the classifier
+def run_test(file_name):
+    with open('test_files/json_sessions_104/' + files.get(file_name), 'r') as file:
         data = file.read()
-        x = json.loads(data, object_hook=lambda d: SimpleNamespace(**d))
-        session = Session(x)
+        session = Session(json.loads(data, object_hook=lambda d: SimpleNamespace(**d)))
         session.buildObject()
-        print("Done")
         clf = None
         with open("RF_new.pkl", 'rb') as f:
             clf = pickle.load(f)
             f.close()
-        key = next(iter(session.sensors))
-        sensor = session.sensors.get(key)
-        readings = sensor.get_readings()
-
-        # readings = readings[9152:9231]
-        print("")
+        readings = session.sensors.get(next(iter(session.sensors))).get_readings()
         jump_count = 0
+
+        print(file_name + ": ")
+        # For each event in the session, find the reading index of the event start and event end.
+        # Pass all readings between those indices to the classifier.
         for event_id in session.events:
             event = session.events.get(event_id)
             start_index = 0
@@ -40,33 +57,13 @@ def main():
             input = tool.preprocess_type(reading_subset)
             jump_count = jump_count + 1
             print("Jump " + str(jump_count) + ": " + str(clf.predict(input)))
+        print("")
 
 
-        #Jump Types:
-        #"axel" = 0;
-        #"toe" = 1;
-        #"flip" = 2;
-        #"lutz" = 3;
-        #"loop" = 4;
-        #"sal" = 5;
-        #"half-loop" = 6;
-        #"waltz" = 7;
-        #print(readings[0].get_magnetometerReading().get_x())
-        # tool = Analyzer()
-        # input = tool.preprocess_type(readings)
-        # for reading in input:
-        #     print(reading)
-        # print(clf.predict(input))
-        # print(session.get_sessionID())
-        # print(session.get_athleteID())
-        # print(session.get_sport())
-        # print(session.get_startTime())
-        # print(session.get_endTime())
-        # print(session.get_status())
-        # print(session.get_deviceID())
-        # print(session.get_sensors())
-        # for a in inspect.getmembers(x):
-        #     print(a)
+def run_all_tests():
+    for file_name in files:
+        run_test(file_name)
+
 
 
 if __name__ == '__main__':
