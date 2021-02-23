4f8de4cb23514687da6b5a63093e4dcffdd647dc
Travisgra13 <39746960+Travisgra13@users.noreply.github.com>
2021-02-23 12:15:55 -0700

    Updated Test

---
+++ test_createTest.py
@@ -0,0 +1,85 @@
+from unittest import TestCase
+from main import createTest
+
+class TestCreateTest(TestCase):
+    #     #Jump Types:
+    #     #"axel" = 0;
+    #     #"toe" = 1;
+    #     #"flip" = 2;
+    #     #"lutz" = 3;
+    #     #"loop" = 4;
+    #     #"sal" = 5;
+    #     #"half-loop" = 6;
+    #     #"waltz" = 7;
+
+    def testak1_09(self):
+        filename = "example_ak1_09.txt"
+        beg = 2690  # 2729
+        end = 2825  # 2805
+        expected = 2
+        prediction = createTest(filename, beg, end, expected)
+        self.assertEqual(expected, prediction)
+
+    def testak1_10(self):
+        filename = "example_ak1_10.txt"
+        beg = 3620  # 3647
+        end = 3750  # 3724
+        expected = 2
+        prediction = createTest(filename, beg, end, expected)
+        self.assertEqual(expected, prediction)
+
+    def testak1_13(self):
+        filename = "example_ak1_13.txt"
+        beg = 75  # 89
+        end = 170  # 151
+        expected = 2
+        prediction = createTest(filename, beg, end, expected)
+        self.assertEqual(expected, prediction)
+
+    def testak1_14(self):
+        filename = "example_ak1_14.txt"
+        beg = 2500  # 2523
+        end = 2600  # 2594
+        expected = 2
+        prediction = createTest(filename, beg, end, expected)
+        self.assertEqual(expected, prediction)
+
+    def testak1_15(self):
+        filename = "example_ak1_15.txt"
+        beg = 1800  # 1813
+        end = 1900  # 1867
+        expected = 4
+        prediction = createTest(filename, beg, end, expected)
+        self.assertEqual(expected, prediction)
+
+    def testak1_17(self):
+        filename = "example_ak1_17.txt"
+        beg = 1800  # 2390
+        end = 1900  # 2459
+        expected = 2
+        prediction = createTest(filename, beg, end, expected)
+        self.assertEqual(expected, prediction)
+
+    def testal1_02(self):
+        filename = "example_al1_02.txt"
+        beg = 1960  # 1994
+        end = 2080  # 2047
+        expected = 0
+        prediction = createTest(filename, beg, end, expected)
+        self.assertEqual(expected, prediction)
+
+    def testcnp_05(self):
+        filename = "example_cnp_05.txt"
+        beg = 1390  # 1406
+        end = 1500  # 1482
+        expected = 3
+        prediction = createTest(filename, beg, end, expected)
+        self.assertEqual(expected, prediction)
+
+    def testcnp_20(self):
+        filename = "example_cnp_20.txt"
+        beg = 610  # 621
+        end = 710  # 698
+        expected = 0
+        prediction = createTest(filename, beg, end, expected)
+        self.assertEqual(expected, prediction)
