339e7bae98033cbd5c867ac241d8ab5b2d265441
Travis Warren Graham <traviswg@tornado.cs.byu.edu>
2021-04-12 19:38:01 -0600

    Created layers

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,72 @@
 #!/bin/sh
 file="../deployment-package.zip"
+batch_1="../dependency_batch_1.zip"
+batch_2="../dependency_batch_2.zip"
+batch_3="../dependency_batch_3.zip"
+batch_4="../dependency_batch_4.zip"
+folder_batch_1="dependencies_batch_1/"
+folder_batch_2="dependencies_batch_2/"
+folder_batch_3="dependencies_batch_3/"
+folder_batch_4="dependencies_batch_4/"
+
+
 directory="./package"
 if [ -f "$file" ] ; then
     rm "$file"
 fi
 
-if [ -f "$directory" ] ; then
+if [ -f "$batch_1" ] ; then
+    rm "$batch_1"
+fi
+
+if [ -f "$batch_2" ] ; then
+    rm "$batch_2"
+fi
+
+if [ -f "$batch_3" ] ; then
+    rm "$batch_3"
+fi
+
+if [ -f "$batch_4" ] ; then
+    rm "$batch_4"
+fi
+
+if [ -d "$directory" ] ; then
     rm -rf "$directory"
 fi
 
-echo "Installing Runtime Dependencies into Package directory."
-pip3 install --upgrade --target ./package cython
-pip3 install --upgrade --target ./package -r requirements.txt
+if [ -d "$folder_batch_1" ] ; then
+    rm -rf "$folder_batch_1"
+fi
+
+if [ -d "$folder_batch_2" ] ; then
+    rm -rf "$folder_batch_2"
+fi
+
+if [ -d "$folder_batch_3" ] ; then
+    rm -rf "$folder_batch_3"
+fi
+
+if [ -d "$folder_batch_4" ] ; then
+    rm -rf "$folder_batch_4"
+fi
+
+
+
+echo "Installing Runtime Dependencies into batch directories."
+pip3 install --no-deps  --upgrade --target ./dependencies_batch_1 -r requirements_batch_1.txt
+pip3 install --no-deps  --upgrade --target ./dependencies_batch_2 -r requirements_batch_2.txt
+pip3 install --no-deps  --upgrade --target ./dependencies_batch_3 -r requirements_batch_3.txt
+pip3 install --no-deps  --upgrade --target ./dependencies_batch_4 -r requirements_batch_4.txt
 echo "Modifying File Access."
 chmod 644 $(find . -type f)
 chmod 755 $(find . -type d)
-echo "Creating zip deployment file."
-zip -r ../deployment-package.zip -r package/
+echo "Creating zip deployment files for dependencies."
+zip -r ../dependency_batch_1.zip -r dependencies_batch_1/
+zip -r ../dependency_batch_2.zip -r dependencies_batch_2/
+zip -r ../dependency_batch_3.zip -r dependencies_batch_3/
+zip -r ../dependency_batch_4.zip -r dependencies_batch_4/
+echo "Creating zip deployment files for project."
 zip -g ../deployment-package.zip main.py
 zip -g ../deployment-package.zip RF_new.pkl
 zip -g ../deployment-package.zip -r orion/
